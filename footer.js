d7d49fc4679a41cbbe8a41375a5907141118d9de
greenpower90 <144276799+greenpower90@users.noreply.github.com>
2024-02-18 19:19:45 +0100

    Add files via upload

--- footer.js
+++ footer.js
@@ -2,6 +2,7 @@ document.getElementById('footer').innerHTML =`
 <p>Petra Pánková</p>
 <p>Zahradnictví Malá Zahrada</p>
 <p>U Mlékárny 290, Mariánské Lázně</p>
+<a href="https://www.mala-zahrada.cz">www.mala-zahrada.cz</a>
 `
 
 
