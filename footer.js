d07b28d0f7d3ca3a8ac19d68476392908cfc096e
greenpower90 <144276799+greenpower90@users.noreply.github.com>
2024-03-07 15:09:54 +0100

    footer update

--- footer.js
+++ footer.js
@@ -3,6 +3,8 @@ document.getElementById('footer').innerHTML =`
 <p>Zahradnictví Malá Zahrada</p>
 <p>U Mlékárny 290, Mariánské Lázně</p>
 <a href="https://www.mala-zahrada.cz">www.mala-zahrada.cz</a>
+<a target="_blank" href="https://www.facebook.com/profile.php?id=100085153880432&paipv=0&eav=AfYE7dPM3SPlOyKZFU0vir8r6E7waDvCqpzbcOAb30d2kACPzr9ApNyiKxF5yszpO_M&_rdr"><img class="icon-small" src="./icons/fb-white.png"></a>
+
 `
 
 
